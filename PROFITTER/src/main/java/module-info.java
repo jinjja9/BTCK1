362f080b75c9cf603e0e91ece6806780af015554
jinjja9 <150103366+jinjja9@users.noreply.github.com>
2024-04-07 20:34:56 +0700

    Delete PROFITTER directory

--- PROFITTER/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.profitter {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-    requires javafx.swing;
-
-    opens com.example.profitter to javafx.fxml;
-    exports com.example.profitter;
-}
